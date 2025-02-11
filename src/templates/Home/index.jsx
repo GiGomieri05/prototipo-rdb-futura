9314ecb91aa66fdf39518cf9dffe4f13067e4680
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2025-02-11 17:58:30 -0300

    update Home component to display a navigation menu and enhance styles

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,5 +1,20 @@
 import './styles.css';
 
 export const Home = () => {
-  return <h1>Olá!</h1>;
+  return (
+    <ul>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Áreas</a>
+      </li>
+      <li>
+        <a>Mentorias</a>
+      </li>
+      <li>
+        <a>Painel</a>
+      </li>
+    </ul>
+  );
 };
