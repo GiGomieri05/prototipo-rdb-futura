8844a38041c02d56005b8e259b4a037b39fbef18
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2025-02-12 15:47:25 -0300

    refactor Navbar component to streamline structure and enhance navigation links

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,37 +1,30 @@
 import './style.css';
-
 import logo from '../../img/logordb.png';
+
+const navLinks = ['Home', 'Áreas', 'Mentorias', 'Painel'];
 
 const Navbar = () => {
   return (
     <nav className="NavBar">
-      <div className="div-img">
-        <img className="logo-rdb" src={logo} alt="Logotipo vermelho da RDB Futura" />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>
-            <a>Home</a>
+      {/* Logo */}
+      <img className="logo-rdb" src={logo} alt="Logotipo vermelho da RDB Futura" />
+
+      {/* Navegação */}
+      <ul className="nav-items">
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <a href="#">{link}</a>
           </li>
-          <li>
-            <a>Áreas</a>
-          </li>
-          <li>
-            <a>Mentorias</a>
-          </li>
-          <li>
-            <a>Painel</a>
-          </li>
-        </ul>
-      </div>
+        ))}
+      </ul>
+
+      {/* Perfil do usuário */}
       <div className="user-profile">
         <div className="info">
           <p className="username">User Profile</p>
           <p className="email">email@email.com</p>
         </div>
-        <div>
-          <img className="user-pic" src="https://picsum.photos/200" alt="Foto de perfil do usuário" />
-        </div>
+        <img className="user-pic" src="https://picsum.photos/200" alt="Foto de perfil do usuário" />
       </div>
     </nav>
   );
